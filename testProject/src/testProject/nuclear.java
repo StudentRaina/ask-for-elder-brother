a06adedb2891d31cd97eeca1fecfd9971a1f0511
태영 <admin@DESKTOP-7GRAO14>
2016-11-14 14:39:08 +0900

    수정4차

--- testProject/src/testProject/nuclear.java
+++ testProject/src/testProject/nuclear.java
@@ -5,7 +5,8 @@ public class nuclear {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("암어 핵");
-sys;
+System.out.println("태영이가 랲을 한다 홍홍홍~");
+
 	}
 
 }
