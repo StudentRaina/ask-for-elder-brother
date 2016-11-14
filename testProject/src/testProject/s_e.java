59f20fdba9f470f352eaa846a0385e33265a234f
student <student@DESKTOP-GI3Q8K2>
2016-11-14 14:13:11 +0900

    asd

--- testProject/src/testProject/s_e.java
+++ testProject/src/testProject/s_e.java
@@ -4,7 +4,7 @@ public class s_e {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("어이~");
+		System.out.println("어이~~~");
 		System.out.println("퍄퍄 보이십니까");
 		System.out.println("><");
 		System.out.println("나도충돌낼래");
