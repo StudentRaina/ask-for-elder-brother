f869d747be16000a86f3c7cc18d82dabf1636aee
student <student@DESKTOP-U83RSI1>
2016-11-14 14:32:42 +0900

    골탕 퍄

--- testProject/src/testProject/s_e.java
+++ testProject/src/testProject/s_e.java
@@ -4,20 +4,7 @@ public class s_e {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("어이~~~");
-		System.out.println("퍄퍄 보이십니까");
-		System.out.println("><");
-		System.out.println("나도충돌낼래");
-
-		System.out.println("충돌끝낫나?~");
-		System.out.println("외쳐 충돌!");
-		System.out.println("외쳐 충돌!");
-		System.out.println("외쳐 충돌!");
-
-		System.out.println("외쳐 충돌!");
-		System.out.println("외쳐 충돌!");
-		System.out.println("수정2");
-		System.out.println("수정3");
+		System.out.println("응 수정없어 응 오류 외쳐 퍄");
 		
 		
 		
