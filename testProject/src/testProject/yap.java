ebf99950b836e71627ae0744e8d0e87a13c6892f
student <student@DESKTOP-GI3Q8K2>
2016-11-14 14:26:39 +0900

    새로 만들자

---
+++ testProject/src/testProject/yap.java
@@ -0,0 +1,13 @@
+package testProject;
+
+public class yap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("새로 만들자!!!!");
+		
+	}
+
+}
