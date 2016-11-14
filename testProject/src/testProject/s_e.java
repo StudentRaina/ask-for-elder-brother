e13121b1d39325825aeee8862e250880ecbf3c2e
태영 <admin@DESKTOP-7GRAO14>
2016-11-14 14:08:45 +0900

    오후 2:08 태영

--- testProject/src/testProject/s_e.java
+++ testProject/src/testProject/s_e.java
@@ -4,7 +4,10 @@ public class s_e {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("어이~~~");
+		System.out.println("어이~~~야호~~");
+		System.out.println("퍄퍄 보이십니까");
+		System.out.println("태영 입니다.");
+
 	}
 
 }
