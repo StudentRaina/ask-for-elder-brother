322f4650bf7f54a4b886c5c938cfbe05f9805c5f
student <student@DESKTOP-U83RSI1>
2016-11-14 15:05:40 +0900

    스리슬쩍 누구도모르게~

--- testProject/src/testProject/s_e.java
+++ testProject/src/testProject/s_e.java
@@ -6,7 +6,7 @@ public class s_e {
 		// TODO Auto-generated method stub
 		System.out.println("응 수정없어 응 오류 외쳐 퍄");
 		System.out.println("우리 오류 확이해볼까?");
-	
+		System.out.println("스리슬쩍~");
 		
 		
 		
