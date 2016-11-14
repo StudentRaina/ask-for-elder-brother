adbe4dc80e01d4054508425a4ef348320a641265
태영 <admin@DESKTOP-7GRAO14>
2016-11-14 14:59:06 +0900

    태영5차

--- testProject/src/testProject/s_e.java
+++ testProject/src/testProject/s_e.java
@@ -2,7 +2,7 @@ package testProject;
 
 public class s_e {
 
-	  abstract static void main(String[] args) {
+	  public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("응 수정없어 응 오류 외쳐 퍄");
 		System.out.println("우리 오류 확이해볼까?");
